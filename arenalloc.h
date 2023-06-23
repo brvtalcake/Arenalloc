afe41d45b4321ba6bf434a6203236ae0f63122ad
Axel PASCON <axlpascon@gmail.com>
2023-06-23 04:33:57 +0200

    Rewrite everything

--- arenalloc.h
+++ arenalloc.h
@@ -26,39 +26,350 @@ SOFTWARE.
 #define ARENALLOC_H
 
 #include <stddef.h>
-#include "tinycthread/source/tinycthread.h"
+//#include "tinycthread/source/tinycthread.h"
 
-#if defined(MAX_DTOR_COUNT)
-    #undef MAX_DTOR_COUNT
+#if defined(ARENA_C)
+    #undef ARENA_C
 #endif
-#define MAX_DTOR_COUNT 32
-
-#if !defined(MAX_ARENA_COUNT)
-    #define MAX_ARENA_COUNT 20
+#if defined(ARENA_CXX)
+    #undef ARENA_CXX
+#endif
+#if defined(ARENA_HAS_FEATURE)
+    #undef ARENA_HAS_FEATURE
+#endif
+#if defined(ARENA_HAS_EXTENSION)
+    #undef ARENA_HAS_EXTENSION
+#endif
+#if defined(ARENA_HAS_INCLUDE)
+    #undef ARENA_HAS_INCLUDE
+#endif
+#if defined(ARENA_WITH_GCC)
+    #undef ARENA_WITH_GCC
+#endif
+#if defined(ARENA_WITH_CLANG)
+    #undef ARENA_WITH_CLANG
+#endif
+#if defined(ARENA_WITH_MSVC)
+    #undef ARENA_WITH_MSVC
+#endif
+#if defined(ARENA_ON_WINDOWS)
+    #undef ARENA_ON_WINDOWS
+#endif
+#if defined(ARENA_ON_UNIX)
+    #undef ARENA_ON_UNIX
+#endif
+#if defined(ARENA_ON_MACOS)
+    #undef ARENA_ON_MACOS
+#endif
+#if defined(ARENA_ON_ANDROID)
+    #undef ARENA_ON_ANDROID
+#endif
+#if defined(ARENA_HAS_THREADS)
+    #undef ARENA_HAS_THREADS
+#endif
+#if defined(ARENA_HAS_ATOMIC)
+    #undef ARENA_HAS_ATOMIC
+#endif
+#if defined(ARENA_ATOMIC_SPECIFIED_TYPE)
+    #undef ARENA_ATOMIC_SPECIFIED_TYPE
+#endif
+#if defined(ARENA_ATOMIC_QUALIFIED_TYPE)
+    #undef ARENA_ATOMIC_QUALIFIED_TYPE
+#endif
+#if defined(ARENA_THREAD_SAFE)
+    #undef ARENA_THREAD_SAFE
 #endif
 
-#if defined(create_region)
-    #undef create_region
+#if defined(__has_feature)
+    #define ARENA_HAS_FEATURE(x) __has_feature(x)
+#else
+    #define ARENA_HAS_FEATURE(x) 0
 #endif
-#define create_region(arena, size, type) create_region_aligned(arena, size, _Alignof(type))
 
-typedef struct arena_region
+#if defined(__has_extension)
+    #define ARENA_HAS_EXTENSION(x) __has_extension(x)
+#else
+    #define ARENA_HAS_EXTENSION(x) 0
+#endif
+
+#if defined(__has_include)
+    #define ARENA_HAS_INCLUDE(x) __has_include(x)
+#else
+    #define ARENA_HAS_INCLUDE(x) 0
+#endif
+
+#if defined(__GNUC__)
+    #define ARENA_WITH_GCC 1
+#elif defined(__clang__)
+    #define ARENA_WITH_CLANG 1
+#elif defined(_MSC_VER)
+    #define ARENA_WITH_MSVC 1
+#else
+    #warning "Unknown compiler"
+#endif
+
+#if defined(__cplusplus)
+    #if defined(ARENA_WITH_MSVC)
+        #if defined(_MSVC_LANG)
+            #if _MSVC_LANG <= 199711L
+                #define ARENA_CXX 1998
+            #elif _MSVC_LANG <= 201103L
+                #define ARENA_CXX 2011
+            #elif _MSVC_LANG <= 201402L
+                #define ARENA_CXX 2014
+            #elif _MSVC_LANG <= 201703L
+                #define ARENA_CXX 2017
+            #else
+                #define ARENA_CXX 2023
+            #endif
+        #else
+            #define ARENA_CXX 1998
+        #endif
+    #else
+        #if __cplusplus <= 199711L
+            #define ARENA_CXX 1998
+        #elif __cplusplus <= 201103L
+            #define ARENA_CXX 2011
+        #elif __cplusplus <= 201402L
+            #define ARENA_CXX 2014
+        #elif __cplusplus <= 201703L
+            #define ARENA_CXX 2017
+        #else
+            #define ARENA_CXX 2023
+        #endif
+    #endif
+#elif defined(__STDC__)
+    #if defined(ARENA_WITH_MSVC)
+        #define ARENA_C 1999
+    #else
+        #if !defined(__STDC_VERSION__)
+            // Assume ANSI C
+            #define ARENA_C 1989
+        #elif __STDC_VERSION__ <= 199409L
+            #define ARENA_C 1994
+        #elif __STDC_VERSION__ <= 199901L
+            #define ARENA_C 1999
+        #elif __STDC_VERSION__ <= 201112L
+            #define ARENA_C 2011
+        #elif __STDC_VERSION__ <= 201710L
+            #define ARENA_C 2017
+        #else
+            #define ARENA_C 2023
+        #endif
+    #endif
+#else
+    #error "Unsupported language"
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+    #define ARENA_ON_WINDOWS 1
+#elif (defined(__linux__) || defined(__unix__)) && !(defined(__APPLE__) || defined(__ANDROID__))
+    #define ARENA_ON_UNIX 1
+#elif defined(__APPLE__)
+    #define ARENA_ON_MACOS 1
+#elif defined(__ANDROID__)
+    #define ARENA_ON_ANDROID 1
+#else
+    #warning "Unknown platform, assuming Unix-like"
+#endif
+
+/*
+ * The conditionnal below could be commented out, since (according to https://en.cppreference.com/w/c/atomic) :
+ *     "If the macro constant __STDC_NO_ATOMICS__(C11) is defined by the compiler, the header <stdatomic.h>, 
+ *      the keyword _Atomic [...] are not provided."
+ * Thus, we shouldn't need to check for the C(++) version, but we'll do it anyway since for example some compilers
+ * are not fully compliant with the standard and thus don't define __STDC_NO_ATOMICS__ even if they don't support
+ * C(++)11 atomics (as another example, MinGW-w64 doesn't define __STDC_NO_THREADS__ even if it doesn't support them,
+ * and doesn't provide <threads.h> (at least used to)).
+ * 
+*/
+
+#if ARENA_C >= 2011 || ARENA_CXX >= 2011
+    #if defined(__STDC_NO_ATOMICS__)
+        #define ARENA_HAS_ATOMIC 0
+        #define ARENA_ATOMIC_SPECIFIED_TYPE(type) type
+        #define ARENA_ATOMIC_QUALIFIED_TYPE(type) type
+    #else
+        #define ARENA_HAS_ATOMIC 1
+        #define ARENA_ATOMIC_SPECIFIED_TYPE(type) _Atomic(type)
+        #define ARENA_ATOMIC_QUALIFIED_TYPE(type) _Atomic type
+        #include <stdatomic.h>
+    #endif
+    // For Windows, prefer the Windows thread API (even with Cygwin's GCC), wich we are sure is available
+    #if defined(__STDC_NO_THREADS__) || defined(ARENA_ON_WINDOWS)
+        #define ARENA_HAS_THREADS 0
+    #else
+        #define ARENA_HAS_THREADS 1
+        #include <threads.h>
+    #endif
+#endif
+
+#if defined(ARENA_ON_WINDOWS)
+    #if ARENA_HAS_ATOMIC == 0
+        // Use Interlocked* functions
+        #define ARENA_HAS_ATOMIC 2
+        #define ARENA_ATOMIC_SPECIFIED_TYPE(type) type
+        #define ARENA_ATOMIC_QUALIFIED_TYPE(type) type
+        #include <windows.h>
+    #endif
+    #if ARENA_HAS_THREADS == 0
+        // Use Windows API
+        #define ARENA_HAS_THREADS 2
+        #include <windows.h>
+    #endif
+#endif
+
+#if defined(ARENA_WITH_CLANG) && (ARENA_HAS_FEATURE(c_atomic) || ARENA_HAS_EXTENSION(c_atomic))
+    #if ARENA_HAS_ATOMIC == 0
+        #if ARENA_HAS_INCLUDE(<stdatomic.h>)
+            #define ARENA_HAS_ATOMIC 1
+            #define ARENA_ATOMIC_SPECIFIED_TYPE(type) _Atomic(type)
+            #define ARENA_ATOMIC_QUALIFIED_TYPE(type) _Atomic type
+            #include <stdatomic.h>
+        #else
+        // Use clang builtins
+            #define ARENA_HAS_ATOMIC 3
+            #define ARENA_ATOMIC_SPECIFIED_TYPE(type) _Atomic(type)
+            #define ARENA_ATOMIC_QUALIFIED_TYPE(type) _Atomic type
+        #endif
+    #endif
+    #if ARENA_HAS_THREADS == 0
+        #if ARENA_HAS_INCLUDE(<threads.h>)
+            #define ARENA_HAS_THREADS 1
+            #include <threads.h>
+        #elif ARENA_HAS_INCLUDE(<pthread.h>)
+            #define ARENA_HAS_THREADS 3
+            #include <pthread.h>
+        #endif
+    #endif
+#endif
+
+#if defined(ARENA_WITH_GCC) && ((defined(ARENA_C) && ARENA_C >= 2011) || (defined(ARENA_CXX) && ARENA_CXX >= 2011))
+    #if ARENA_HAS_ATOMIC == 0
+    // Use gcc builtins, that needs C(++)11 memory model
+        #define ARENA_HAS_ATOMIC 4
+        #define ARENA_ATOMIC_SPECIFIED_TYPE(type) type
+        #define ARENA_ATOMIC_QUALIFIED_TYPE(type) type
+    #endif
+#endif
+#if defined(ARENA_WITH_GCC)
+    #if ARENA_HAS_THREADS == 0
+        #if ARENA_HAS_INCLUDE(<threads.h>)
+            #define ARENA_HAS_THREADS 1
+            #include <threads.h>
+        #elif ARENA_HAS_INCLUDE(<pthread.h>)
+            #define ARENA_HAS_THREADS 3
+            #include <pthread.h>
+        #endif
+    #endif
+#endif
+
+#if ARENA_HAS_ATOMIC == 0
+    #if ARENA_HAS_INCLUDE(<stdatomic.h>)
+        #define ARENA_HAS_ATOMIC 1
+        // Having the include doesn't mean we have the _Atomic keyword
+        #define ARENA_ATOMIC_SPECIFIED_TYPE(type) type
+        #define ARENA_ATOMIC_QUALIFIED_TYPE(type) type
+        #include <stdatomic.h>
+    #endif
+#endif
+
+#if ARENA_HAS_THREADS == 0
+    #if ARENA_HAS_INCLUDE(<threads.h>)
+        #define ARENA_HAS_THREADS 1
+        #include <threads.h>
+    #elif ARENA_HAS_INCLUDE(<pthread.h>)
+        #define ARENA_HAS_THREADS 3
+        #include <pthread.h>
+    #endif
+#endif
+
+#if ARENA_HAS_ATOMIC == 0 && ARENA_HAS_THREADS == 0
+    #define ARENA_THREAD_SAFE 0
+#else
+    #define ARENA_THREAD_SAFE 1
+#endif
+
+#include <stdint.h>
+
+#if ARENA_THREAD_SAFE == 1
+    #if ARENA_HAS_ATOMIC != 0
+        #if ARENA_HAS_ATOMIC == 1
+        // Use C(++)11 atomics
+            #define ARENA_LOCK_TYPE volatile ARENA_ATOMIC_QUALIFIED_TYPE(int16_t)
+            #define ARENA_LOCK_INIT 0
+            #define ARENA_LOCK_DESTROY(lock)
+            #define ARENA_LOCK(lock)                                                    \
+                {                                                                       \
+                    ARENA_LOCK_TYPE expected = 0;                                       \
+                    ARENA_LOCK_TYPE desired = 1;                                        \
+                    while(!atomic_compare_exchange_weak_explicit(                       \
+                            &(lock),                                                    \
+                            &expected,                                                  \
+                            desired,                                                    \
+                            memory_order_acquire,                                       \
+                            memory_order_relaxed))                                      \
+                    {                                                                   \
+                        expected = 0;                                                   \
+                        desired = 1;                                                    \
+                    }                                                                   \
+                }
+            #define ARENA_UNLOCK(lock) atomic_store_explicit(&(lock), 0, memory_order_release)
+        #elif ARENA_HAS_ATOMIC == 2
+        // Use Windows Interlocked* functions
+            #define ARENA_LOCK_TYPE volatile ARENA_ATOMIC_QUALIFIED_TYPE(int16_t)
+            #define ARENA_LOCK_INIT 0
+            #define ARENA_LOCK_DESTROY(lock)
+            #define ARENA_LOCK(lock) while(InterlockedCompareExchange16(&(lock), 1, 0))
+            #define ARENA_UNLOCK(lock) InterlockedExchange16(&(lock), 0)
+        #elif ARENA_HAS_ATOMIC == 3
+        // Use clang builtins
+            #define ARENA_LOCK_TYPE volatile ARENA_ATOMIC_QUALIFIED_TYPE(int16_t)
+            #define ARENA_LOCK_INIT 0
+            #define ARENA_LOCK_DESTROY(lock)
+            #define ARENA_LOCK(lock) while(__c11_atomic_compare_exchange_weak(&(lock), 0, 1, memory_order_acquire, memory_order_relaxed))
+            #define ARENA_UNLOCK(lock) __c11_atomic_store(&(lock), 0, memory_order_release)
+        #elif ARENA_HAS_ATOMIC == 4
+        // Use gcc builtins
+            /*
+             * TODO
+             */
+        #endif
+    #elif ARENA_HAS_THREADS == 1
+    // Use C11 threads
+        /*
+         * TODO
+         */
+    #elif ARENA_HAS_THREADS == 2
+    // Use Windows API
+        /*
+         * TODO
+         */
+    #elif ARENA_HAS_THREADS == 3
+    // Use pthread
+        /*
+         * TODO
+         */
+    #endif
+#else
+    #include <signal.h>
+    #define ARENA_LOCK_TYPE volatile sig_atomic_t
+    #define ARENA_LOCK_INIT 0
+    #define ARENA_LOCK_DESTROY(lock)
+    #define ARENA_LOCK(lock) while((lock))
+    #define ARENA_UNLOCK(lock) (lock) = 0
+#endif
+
+typedef struct Arena
 {
-    size_t padding_left;
-    size_t padding_right;
-    size_t start_pos;
-    size_t end_pos;
-    size_t alignment;
-} arena_region;
+    struct Arena* next;
+    char* limit;
+    char* avail;
+    ARENA_LOCK_TYPE lock;
+} arena_t;
 
-typedef struct arena 
-{
-    size_t region_count;
-    void* memory_start;
-    void* memory_end;
-    arena_region *first_region;
-    arena_region *last_region;
-    mtx_t lock;
-}arena;
+
+
+
 
 #endif // ARENALLOC_H
